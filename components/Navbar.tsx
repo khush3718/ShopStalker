fdcb97f0c33b270c49ee66d31445bebd7efb4e88
Khush Shah <khushshah51@gmail.com>
2023-10-21 15:29:59 +0530

    added logo and a few elements to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,40 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
+
+const navIcons = [
+    { src: '/assets/icons/search.svg', alt: 'search' },
+    { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+    { src: '/assets/icons/user.svg', alt: 'user' },
+]
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <header className='w-full'>
+        <nav className='nav'>
+            <Link href="/" className='flex items-center gap-1'>
+                <Image src='/assets/icons/logo.svg' width={30} height={30} alt='logo' />
+                <p className='nav-logo'>
+                    Shop<span className='text-primary'>Stalker</span>
+                </p>
+            </Link>
+
+            <div className='flex items-center gap-5'>
+                {navIcons.map((icon, index) => (
+                    <Image
+                        key={icon.alt}
+                        src={icon.src}
+                        alt={icon.alt}
+                        width={30}
+                        height={30}
+                        className='object-contain'
+                    />
+                )
+                )}
+            </div>
+
+        </nav>
+   </header>
   )
 }
 
