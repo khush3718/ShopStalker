e2c7c1dc6dd7b660a6fa758f042498ddda88d937
Khush Shah <khushshah51@gmail.com>
2023-10-22 18:28:20 +0530

    added some logic to the searchbar

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,16 +1,70 @@
 "use client"
+import { useState, FormEvent } from 'react'
+
+const isValidAmazonLink = (url: string) => {
+  try {
+    const parsedURL = new URL(url)
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes('amazon.com') ||
+      hostname.includes('amazon.') ||
+      hostname.endsWith('amazon')
+    ) {
+      return true;
+    }
+
+  } catch (error) {
+    return false
+  }
+  return false
+}
+
 
 const Searchbar = () => {
-    const handleSubmit = () => {}
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonLink(searchPrompt);
+
+    if(!isValidLink) return alert('Please provide a valid Amazon link')
+
+    try {
+      setIsLoading(true);
+
+      // Scrape the product page
+      // const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-   <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
-        <input
-            type="text"
-            placeholder="Enter the amazon product link here.."
-            className='searchbar-input'
-        />
-        <button type="submit" className='searchbar-btn'>Search</button>
-   </form>
+    <form 
+      className="flex flex-wrap gap-4 mt-12" 
+      onSubmit={handleSubmit}
+    >
+      <input 
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Paste your Amazon link here"
+        className="searchbar-input"
+      />
+
+      <button 
+        type="submit" 
+        className="searchbar-btn"
+        disabled={searchPrompt === ''}
+      >
+        {isLoading ? 'Searching...' : 'Track'}
+      </button>
+    </form>
   )
 }
 
