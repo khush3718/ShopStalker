d3a891434a827d73abca8d31a2ead7344b381e25
Khush Shah <khushshah51@gmail.com>
2023-11-06 15:43:49 +0530

    edited page

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
-                Smart Shopping starts here:
+                Smart Shopping starts here
                 <Image 
                   src="/assets/icons/arrow-right.svg"
                   alt="arrow-right"
